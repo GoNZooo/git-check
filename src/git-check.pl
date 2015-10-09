330684bdb5f7ae10fdee350e0ead0ef2e6200ce1
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-10 02:56:20 +0300

    First commit

---
+++ src/git-check.pl
@@ -0,0 +1,44 @@
+#!/home/gonz/.rakudobrew/bin/perl6
+
+use v6;
+
+sub dirs-in-dir($path) {
+    my @dirs;
+    for dir($path) -> $d {
+        $d.d ?? @dirs.push: $d !! next;
+    }
+    @dirs;
+}
+
+sub has-git-dir($path) {
+    for dir($path) -> $d {
+        $d.d && $d.Str ~~ /".git"/ ?? return True !! next;
+    };
+    False;
+}
+
+sub get-git-status($path) {
+    my $curdir = $*CWD;
+    chdir($path);
+    my $git-output = run("git", "status", :out).out.slurp-rest;
+    chdir($curdir);
+    $git-output;
+}
+
+sub git-not-changed($git-output) {
+    so $git-output ~~ /"nothing to commit, working directory clean"/;
+}
+
+sub check-dir($path) {
+    if has-git-dir($path) {
+        if !git-not-changed get-git-status($path) {
+            say $path.Str, " should be checked."; 
+        }
+    } else {
+        for dirs-in-dir($path) -> $d {
+            check-dir($d);
+        }
+    }
+}
+
+check-dir(".");
